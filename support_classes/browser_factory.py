9c2d206f370dbf7b315ad142b8cdeb083231ba49
CarlosRG <cramirez.griego@gmail.com>
2022-08-18 15:02:13 -0700

    Logic to navigate to National Dex working

--- support_classes/browser_factory.py
+++ support_classes/browser_factory.py
@@ -1,5 +1,7 @@
 from .system_enums import TargetBrowser
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 class BrowserFactory:
 
@@ -7,10 +9,8 @@ class BrowserFactory:
     browser: TargetBrowser = TargetBrowser.Chrome
     driver = ""
 
-    def __init__(self, tb: str):
+    def __init__(self):
         self.name = "NoName"
-        self.browser = TargetBrowser
-        self.SetBrowserByString(tb)
 
 
     def SetBrowserByString(self, targetBrowser: str):
@@ -46,3 +46,45 @@ class BrowserFactory:
 
     def NavigateToThisPage(self, url: str):
         self.driver.get(url)
+
+
+    def SearchElements(self, method: str, selector: str, waitSize: int, intervals: int, expected: bool):
+        cycles: int = 0
+        listElements = []
+        dataFound: bool = False
+        if expected:
+            while cycles < intervals:
+                if method.lower() == "css":
+                    listElements = self.driver.find_elements(By.CSS_SELECTOR, selector)
+                elif method.lower() == "xpath":
+                    listElements = self.driver.find_elements(By.XPATH, selector)
+                elif method.lower() == "id":
+                    listElements = self.driver.find_elements(By.ID, selector)
+                else:
+                    listElements = self.driver.find_elements(By.CSS_SELECTOR, selector)
+                if len(listElements) > 0:
+                    dataFound = True
+                    break
+                else:
+                    time.sleep(waitSize)
+                    cycles = cycles + 1
+        else:
+            while cycles < intervals:
+                if method.lower() == "css":
+                    listElements = self.driver.find_elements(By.CSS_SELECTOR, selector)
+                elif method.lower() == "xpath":
+                    listElements = self.driver.find_elements(By.XPATH, selector)
+                elif method.lower() == "id":
+                    listElements = self.driver.find_elements(By.ID, selector)
+                else:
+                    listElements = self.driver.find_elements(By.CSS_SELECTOR, selector)
+                if len(listElements) > 0:
+                    time.sleep(waitSize)
+                    cycles = cycles + 1
+                else:
+                    dataFound = True
+                    break
+        if not dataFound:
+            print("The selector didnt return the expected results: "+selector)
+            print("Were elements expected: "+expected)
+        return listElements
