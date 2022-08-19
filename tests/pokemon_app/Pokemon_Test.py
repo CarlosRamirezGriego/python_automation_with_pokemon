1b2385a01982fa677b2b5c36b460e6b4459d5794
CarlosRG <cramirez.griego@gmail.com>
2022-08-18 18:07:26 -0700

    Added UI Test sample

--- tests/pokemon_app/Pokemon_Test.py
+++ tests/pokemon_app/Pokemon_Test.py
@@ -4,6 +4,10 @@ import sys
 sys.path.append('../python_automation_with_pokemon')
 
 from business_logic.api.PokemonLogic import PokemonLogic
+from support_classes.browser_factory import BrowserFactory
+from business_logic.ui.MainPage import MainPageLogic
+from business_logic.ui.NationalDexPage import NationalDexPage
+from support_classes.system_enums import TargetBrowser
 
 
 def test_exampleForAPI():
@@ -12,4 +16,25 @@ def test_exampleForAPI():
     pkName: str = apiLogic.returnNameOfPokemonWithThisNumber(25)
     assert pkNumber == 25
     assert pkName == "pikachu"
+
+
+
+def test_exampleForUI():
+    browserClass = BrowserFactory()
+    browserClass.SetBrowserByEnum(TargetBrowser.Chrome)
+    browserClass.MaximizeBrowser()
+    browserClass.NavigateToThisPage("https://pokemondb.net/")
+
+    mainPage = MainPageLogic(browserClass.driver)
+    mainPage.NavigateToPokedexFromQuickLinksMenu()
+
+    nationPage = NationalDexPage(browserClass.driver)
+    namePresent: bool = nationPage.DoesAPokemonWithThisNameExist("Pikachu")
+    numPresent: bool = nationPage.DoesAPokemonWithThisNumberExist(25)
+    name: str = nationPage.ReturnNameOfPokemonWithThisNumber(25)
+    dexNum: int = nationPage.ReturnNumberOfPokemonWithThisName("Pikachu")
+    assert namePresent == True
+    assert numPresent == True
+    assert name == "Pikachu"
+    assert dexNum == 25
 
